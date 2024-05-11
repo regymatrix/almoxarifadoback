7110da6037e469ff3bdc88d500372e2b04b77933
viniciuss1812 <regymatrix@gmail.com>
2024-05-11 11:48:46 -0300

    implementando automapper

--- src/srcBackAlmoxarifadoASPCore/AlmoxarifadoServices/GrupoService.cs
+++ src/srcBackAlmoxarifadoASPCore/AlmoxarifadoServices/GrupoService.cs
@@ -1,6 +1,8 @@
 ﻿using AlmoxarifadoDomain.Models;
 using AlmoxarifadoInfrastructure.Data.Interfaces;
 using AlmoxarifadoServices.DTO;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,19 +14,30 @@ namespace AlmoxarifadoServices
     public class GrupoService
     {
         private readonly IGrupoRepository _grupoRepository;
+        private readonly MapperConfiguration configurationMapper;
 
         public GrupoService(IGrupoRepository pGrupoRepository)
         {
             _grupoRepository = pGrupoRepository;
+            configurationMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Grupo, GrupoGetDTO>();
+                cfg.CreateMap<GrupoGetDTO, Grupo>();
+            });
         }
 
-        public List<Grupo> ObterTodosGrupos()
+        public List<GrupoGetDTO> ObterTodosGrupos()
         {
-            return _grupoRepository.ObterTodosGrupos();
+            var mapper = configurationMapper.CreateMapper();
+
+
+            return  mapper.Map<List<GrupoGetDTO>>(_grupoRepository.ObterTodosGrupos());
         }
 
         public Grupo ObterGrupoPorID(int id)
         {
+            
+
             return _grupoRepository.ObterGrupoPorId(id);
         }
 
